5dfe187cb08313f227795553213d3801ee87bae5
Luke Logan <llogan@highpoint.edu>
2019-06-18 05:43:06 -0400

    adds promises

--- exercism-ideas.js
+++ exercism-ideas.js
@@ -1,3 +1,19 @@
 for(let i = 0; i <= cuts.length; i++){
     console.log(cuts[i]);
 }
+
+for(const index in cuts ){
+    console.log(index[cuts]);
+}
+
+  // You can destructure this immediately
+  for (const [i, cut] of cuts.entries()) {
+    console.log(`${cut} is the ${i + 1} item`);
+  }
+
+//   for...in will use the index
+
+// Array.from will allow for you to MAP over the data
+
+// This will give a name to each item in the array
+// const [letter1, letter2, letter3] = letters;
